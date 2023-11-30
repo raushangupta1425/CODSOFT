47ff9029638e395b3b9ab9422c9bb284b56aa72b
Raushan Gupta <126952814+raushangupta1425@users.noreply.github.com>
2023-11-30 13:35:14 +0000

    backend

--- Level_1/Task_3:Calculator/script.js
+++ Level_1/Task_3:Calculator/script.js
@@ -4,7 +4,7 @@ let equalsButton = document.querySelector('[equl]');
 let clearButton = document.querySelector('[clear]');
 let deletButton = document.querySelector('[delet]');
 let previousButton = document.getElementById("previous");
-let currentButton = document.getElementById("current");
+let currentButton = document.getElementById("current");         // for Recently typed number
 
 numberButtons.forEach(button => {
     button.addEventListener('click', () =>{ 
@@ -27,10 +27,50 @@ operationButtons.forEach(button => {
 })
 clearButton.addEventListener('click', button =>{ 
     currentButton.innerHTML = "0";
+    previousButton.innerHTML = "";
     currentButton.style.color = "#ccc"
 })
 deletButton.addEventListener('click', button =>{ 
-    var str = currentButton.innerHTML;
-    str.pop(1);
-    currentButton.innerHTML = str;
+    previousButton.innerHTML = "";
+    currentButton.innerHTML = currentButton.innerHTML.toString().slice(0, -1);
 })
+
+equalsButton.addEventListener('click', button =>{ 
+    previousButton.innerHTML = currentButton.innerHTML;
+    let combineString = currentButton.innerHTML.toString();
+    currentButton.innerHTML = eval(combineString);
+    // var operands = combineString.split("+");
+    // var operands = combineString.split("-");
+    // var operands = combineString.split("*");
+    // alert(operands[1]);
+    // var res = compute(operands[0],operands[1]);
+    // alert(res);
+    // currentButton.innerHTML = res;
+})
+// var operation = '+';
+// function compute(prev, current){
+//     let computation;
+//     if(currentButton.toString.includes('+' || '-' || '*' || '/')){
+//             operation = currentButton.toString().slice(length/2,(length/2)+1)
+//         }
+//     let first = parseFloat(prev);
+//     let sec = parseFloat(current);
+//     alert(typeof first)
+//     switch(operation) {
+//         case '+':
+//             computation = first + sec;
+//             break;
+//         case '-':
+//             computation = prev - current;
+//             break;
+//         case '*':
+//             computation = prev * current;
+//             break;
+//         case '/':
+//             computation = prev / current;
+//             break;
+//         default:
+//             operation = undefined;
+//         return computation;
+//     }
+// }
